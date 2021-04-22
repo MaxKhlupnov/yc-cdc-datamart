c50990b13e6b7d73a716bd1987d90db3c5eda1e7
khlupnov <m.khlupnov@gmail.com>
2021-04-22 22:45:41 +0300

    1.4

--- debezium-cdc/build.sh
+++ debezium-cdc/build.sh
@@ -9,20 +9,18 @@ sudo apt install openjdk-11-jre-headless
 #
 # create trusted CA store and add YandexCA.crt as trusted root
 
-wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" -O ./yc-connect/1.6/YandexCA.crt
-
-keytool -keystore ./yc-connect/1.6/client.truststore.jks -storepass 'pass@word1'  -alias CARoot -import -file ./yc-connect/1.6/YandexCA.crt
-
+#wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" -O ./yc-connect/1.6/YandexCA.crt
+#keytool -keystore ./yc-connect/1.6/client.truststore.jks -storepass 'pass@word1'  -alias CARoot -import -file ./yc-connect/1.6/YandexCA.crt
 # create trusted CA store and add YandexCA.crt as trusted root
-docker build -t debezium/kafka:1.6 -f ./kafka/Dockerfile kafka/1.6/.
-docker build -t debezium/connect-base:1.6 -f ./connect-base/Dockerfile connect-base/1.6/.
-docker build -t debezium/connect:1.6 -f ./connect/Dockerfile connect/1.6/.
-docker build -t yc/connect:1.6 -f ./yc-connect/Dockerfile yc-connect/1.6/.
+#docker build -t debezium/kafka:1.6 -f ./kafka/1.6/Dockerfile kafka/1.6/.
+#docker build -t debezium/connect-base:1.6 -f ./connect-base/1.6/Dockerfile connect-base/1.6/.
+#docker build -t debezium/connect:1.6 -f ./connect/1.6/Dockerfile connect/1.6/.
+#docker build -t yc/connect:1.6 -f ./yc-connect/1.6/Dockerfile yc-connect/1.6/.
 
 wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" -O ./yc-connect/1.4/YandexCA.crt
 keytool -keystore ./yc-connect/1.4/client.truststore.jks -storepass 'pass@word1'  -alias CARoot -import -file ./yc-connect/1.4/YandexCA.crt
 
-docker build -t debezium/kafka:1.4 -f ./kafka/Dockerfile kafka/1.4/.
-docker build -t debezium/connect-base:1.4 -f ./connect-base/Dockerfile connect-base/1.4/.
-docker build -t debezium/connect:1.4 -f ./connect/Dockerfile connect/1.4/.
-docker build -t yc/connect:1.4 -f ./yc-connect/Dockerfile yc-connect/1.4/.
+#docker build -t debezium/kafka:1.4 -f ./kafka/1.4/Dockerfile kafka/1.4/.
+#docker build -t debezium/connect-base:1.4 -f ./connect-base/1.4/Dockerfile connect-base/1.4/.
+#docker build -t debezium/connect:1.4 -f ./connect/1.4/Dockerfile connect/1.4/.
+docker build -t yc/connect:1.4 -f ./yc-connect/1.4/Dockerfile yc-connect/1.4/.
