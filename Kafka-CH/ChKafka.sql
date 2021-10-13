d0f8b403cfc9d97ea0aba72399f46736c939e0e5
khlupnov <m.khlupnov@gmail.com>
2021-10-13 19:23:40 +0300

    store_location

--- Kafka-CH/ChKafka.sql
+++ Kafka-CH/ChKafka.sql
@@ -2,6 +2,7 @@ CREATE TABLE kafka_store_data  (
 	 store_id  UInt32,
 	 store_name String,
 	 store_address Nullable(String),
+	 store_location Nullable(String),
 	 description Nullable(String)
 ) ENGINE = Kafka SETTINGS kafka_broker_list = '<kafka-broker-fqdn>:9091',
                             kafka_topic_list = 'inventory.dbo.store_data',
@@ -9,19 +10,17 @@ CREATE TABLE kafka_store_data  (
                             kafka_format = 'JSONEachRow'
 
 
-{"store_id":10050,"store_name":"STORE_10050","store_address":"обл Московская,г Жуковский,ул Фрунзе,д 28","description":""}
-
-
 CREATE TABLE ch_store_data  ON CLUSTER  '{cluster}'(
 	 store_id  UInt32,
 	 store_name String,
 	 store_address Nullable(String),
+	 store_location Nullable(String),
 	 description Nullable(String)
 ) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{shard}/ch_store_data', '{replica}') 
 ORDER BY (store_id)
 
 CREATE MATERIALIZED VIEW materialized_store_data TO store_data
-		AS SELECT  store_id, store_name, store_address,	 description
+		AS SELECT  store_id, store_name, store_address, store_location, description
 FROM kafka_store_data
 
 
